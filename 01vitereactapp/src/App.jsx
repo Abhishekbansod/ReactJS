f998e0d98a39525bbb92d8aa25d9e641c33b7423
Abhishekbansod <bansodabhishek7@gmail.com>
2024-09-11 16:15:01 +0530

    basic react app using vite bundler

---
+++ 01vitereactapp/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./Test"
+
+function App() {
+  return (
+    <div>
+      <Test/>
+      <h1>Learning React JS with vite bundler from App!</h1>
+    </div>    
+  )
+}
+
+export default App
